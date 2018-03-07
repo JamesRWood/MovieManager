609f43e93c9d649c9880314bc5e53e4b1e5b057a
James Wood <James.Wood@moonpig.com>
2018-03-07 17:19:41 +0000

    UI updates

---
+++ MovieManager/Converters/ScreenResolutionToPageWidthConverter.cs
@@ -0,0 +1,31 @@
+﻿namespace MovieManager.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class ScreenResolutionToPageWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var width = 1280.00;
+            if (parameter is double)
+            {
+                var screenWidth = (double)parameter;
+                if (screenWidth > width)
+                {
+                    return width;
+                }
+
+                width = screenWidth * 0.8;
+            }
+
+            return (int)width;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
