91fc1a70cd4c2f3f9fefef9cd49fccc82aa39467
JWood <j.r.wood@hotmail.co.uk>
2018-04-03 21:35:32 +0100

    Further updates to edit movie settings logic

--- MovieManager/Commands/RelayCommands/SetSelectedMovieCommand.cs
+++ MovieManager/Commands/RelayCommands/SetSelectedMovieCommand.cs
@@ -1,20 +1,26 @@
 ﻿namespace MovieManager.Commands.RelayCommands
 {
-    using System;
-    using System.Windows;
-    using System.Windows.Input;
     using Contracts.ViewModels;
     using Models;
     using MovieManager.Contracts.Commands.RelayCommands;
+    using MovieManager.Contracts.Controllers;
+    using MovieManager.Helpers;
+    using System;
+    using System.Windows;
+    using System.Windows.Input;
 
     public class SetSelectedMovieCommand : ISetSelectedMovieCommand
     {
         private readonly ICommonDataViewModel _commonData;
+        private readonly IFileController _fileController;
         private ICommand _command;
 
-        public SetSelectedMovieCommand(ICommonDataViewModel commonData)
+        public SetSelectedMovieCommand(
+            ICommonDataViewModel commonData, 
+            IFileController fileController)
         {
             _commonData = commonData;
+            _fileController = fileController;
         }
 
         public ICommand Command => _command ?? (_command = new RelayCommand<object>(Execute, CanExecute));
@@ -22,21 +28,48 @@
         public void Execute(object parameter)
         {
             var val = (object[]) parameter;
-            if (val[0] is Movie selectedMovie)
+            if (val.Length == 3 &&
+                val[1] is Movie updatedSelectedMovie &&
+                val[2] is Movie selectedMovie)
             {
-                _commonData.CommonDataSelectedMovie = selectedMovie;
+                UpdateMovies(updatedSelectedMovie, selectedMovie);
             }
 
-            if (val[1] is Window window)
+            if (!(val[0] is Window window))
             {
-                window.Close();
+                return;
             }
+
+            window.Close();
+            Application.Current.MainWindow.ToggleBlurEffect();
         }
 
         public bool CanExecute(object parameter)
         {
             var val = (object[])parameter;
-            return val?[0] is Movie;
+            return val?[1] is Movie;
+        }
+
+        private void UpdateMovies(Movie updatedSelectedMovie, Movie selectedMovie)
+        {
+            if (updatedSelectedMovie == null)
+            {
+                return;
+            }
+
+            updatedSelectedMovie.FileLocation = _fileController.RenameFile(selectedMovie.FileLocation, updatedSelectedMovie.Title);
+
+            var movies = _commonData.CommonDataMovies;
+            movies.Remove(selectedMovie);
+            movies.Add(updatedSelectedMovie);
+
+            _fileController.StoreMovieData(movies);
+
+            _commonData.CommonDataMovies = null;
+            _commonData.CommonDataMovies = movies;
+
+            _commonData.CommonDataSelectedMovie = null;
+            _commonData.CommonDataSelectedMovie = updatedSelectedMovie;
         }
 
         public event EventHandler CanExecuteChanged;
---
+++ MovieManager/Helpers/WindowEffectHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace MovieManager.Helpers
+{
+    using System.Windows;
+    using System.Windows.Media.Effects;
+
+    public static class WindowEffectHelper
+    {
+        public static void ToggleBlurEffect<T>(this T window) where T : Window
+        {
+            BlurEffect blur;
+            var hasBlurEffect = window.Effect is BlurEffect;
+            if (!hasBlurEffect)
+            {
+                blur = new BlurEffect
+                {
+                    Radius = 50,
+                    RenderingBias = RenderingBias.Quality
+                };
+            }
+            else
+            {
+                blur = null;
+            }
+
+            window.Effect = blur;
+        }
+    }
+}
