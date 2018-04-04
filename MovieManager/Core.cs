23f58f5d8d210d0f3238f139161bfbad64bc4121
James Wood <James.Wood@moonpig.com>
2018-04-04 15:21:56 +0100

    Fade in background effect (only kinda working)

--- MovieManager/Core.cs
+++ MovieManager/Core.cs
@@ -1,11 +1,14 @@
 ﻿namespace MovieManager
 {
     using System.Collections.Generic;
+    using System.Windows.Media;
 
     public static class Core
     {
         public static string DateTagRegex => @"\s\[[0-9]{4}\]|\[[0-9]{4}\]|\s\([0-9]{4}\)|\([0-9]{4}\)|\s[0-9]{4}";
 
         public static List<string> MovieFileTypes => new List<string> { ".mp4", ".avi" };
+
+        public static Color TransparentColor => Color.FromArgb(0, 0, 0, 0);
     }
 }
